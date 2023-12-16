f18fd94b4a18b4174976dd000e5296f3b3cf426d
Claire <clairefraser121@gmail.com>
2023-12-16 22:18:43 +0000

    added content to generateMarkdown

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,7 +1,38 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectTitle}
 
+  ## Description
+  ${data.projectDescription}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  This project is licensed under the ${data.license} License.
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  If you have any questions, feel free to reach out to me on GitHub:
+  [${data.githubUsername}](https://github.com/${data.githubUsername})
+
+  You can also contact me via email:
 `;
 }
 
