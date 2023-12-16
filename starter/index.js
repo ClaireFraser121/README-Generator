7c764ae0b249eec13202e975d29932bec866bd76
Claire <clairefraser121@gmail.com>
2023-12-16 22:06:32 +0000

    added functions to the index.js

--- starter/index.js
+++ starter/index.js
@@ -8,22 +8,49 @@ const questions = [
     {
         type: 'input',
         name: 'projectTitle',
-        message: 'What is the title of your project?'
+        message: 'What is the title of your project?',
     },
     {
         type: 'input',
         name: 'projectDescription',
         message: 'Provide a brief description of your project:',
     },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installtion instructions for your project?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your application:',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can others contribute to your project?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address? (For users to reach out with additional questions)',
+    },
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+    console.log( 'File "${fileName}" written successfully!');
 }
 
 // function to initialize program
 function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const markdownContent = generateMarkdown(answers);
+        writeToFile('READ.md', markdownContent);
+    });
 
 }
 
